63b00b08d4a93c460d30813ec0a99e53fe745c2f
ErickRios77 <erick.rios@sptech.school>
2024-06-03 23:54:19 -0300

    Estilização das páginas de home, cadastro, login e home do usuário logado

--- public/js/home.js
+++ public/js/home.js
@@ -5,13 +5,13 @@ function listarVtubers(){
         resposta.json().then((vtubers)=>{
             vtubers.forEach((vtuber) => {
                 containerCards.innerHTML+=`
-                    <div style="border: 3px solid #000000; border-radius:15px; padding:5px; display: flex; flex-direction: column; position:relative; padding-bottom:10px">
-                        <img src="..${vtuber.modelVtuber}" width="100%" height="200px" style="border-radius:5px">
-                        <h3 id="${vtuber.idVtuber}" style="overflow-wrap: break-word; margin-bottom:40px">
+                    <div class="cards">
+                        <img src="..${vtuber.modelVtuber}" class="cardImg">
+                        <h3 id="${vtuber.idVtuber}">
                             ${vtuber.nomeVtuber}
-                            <img src="../${vtuber.logoAgencia}" style="height: 2rem; position: relative; bottom: -7px;">
+                            <img src="../${vtuber.logoAgencia}" class="agencia">
                         </h3>
-                        <button onclick="paginaVtuber(${vtuber.idVtuber})" style="position: absolute; bottom: 10px; align-self:center;">Saiba Mais</button>
+                        <button onclick="paginaVtuber(${vtuber.idVtuber})">Saiba Mais</button>
                     </div>
                 `
             });
